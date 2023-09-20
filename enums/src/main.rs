aeb8a275213b3909af06e8688f2fe48d90528c46
EleyondFarli <sami.gherasim@gmail.com>
2023-09-20 19:31:07 +0300

    patterns

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -1,3 +1,5 @@
+use std::os::linux::raw::stat;
+
 enum IpAddr {
     V4(u8, u8, u8, u8),
     V6(String),
@@ -16,6 +18,41 @@ impl Message {
     }
 }
 
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+fn value_from_coin(coin: Coin) -> u32 {
+    match coin {
+        Coin::Penny => {
+            println!("Lucky Penny!");
+            1
+        },
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}", state);
+            25
+        },
+    }
+}
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        Some(val) => Some(val + 1),
+        None => None,
+    }
+}
+
 fn main() {
     let home = IpAddr::V4(127, 0, 0, 1);
     let loopback = IpAddr::V6(String::from("::1"));
@@ -25,4 +62,8 @@ fn main() {
     let some_number = Some(5);
     let some_char = Some('e');
     let absent_number: Option<i32> = None;
+
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
 }
